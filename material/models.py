82aa1e76d37ec9d22694a486ff6af30c847745a9
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-14 19:53:16 -0400

    feat(material): added Course and Material models

--- material/models.py
+++ material/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    """ Course: 
+            Representa los Ramos disponibles
+        - name: Nombre del ramo
+    """
+    name  = models.CharField(max_length=50)
+
+
+class Material(models.Model):
+    """ Material:
+            Representa los materiales disponibles
+        - name: nombre del material
+        - path: ruta del archivo
+        - course: Ramo del material
+    """
+    name  = models.CharField(max_length=50)
+    path  = models.CharFiled(max_lenght=50)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
