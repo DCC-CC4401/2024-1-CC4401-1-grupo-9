3dc9f32f716648ba2fed0f90d27724448fedec2a
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-16 16:40:49 -0400

    refact(materials): change in the view

--- material/views.py
+++ material/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from material.models import Course, Material
 
-# Create your views here.
-def material(request):
-    materials = Material.objects.all()
-    courses = Course.objects.all()
+
+def material(request, material_id: int = None):
+    """ View para los materiales.
+
+        GET: 
+        Permite obtener información de los materiales,
+        o de un solo material.
+    """
 
     if request.method == "GET":
-        return render(
-            request=request, 
-            template_name='materials.html', 
-            context={"materials": materials, "courses": courses})
+        ## Renderizar pagina de Materiales
+        if material_id is None:
+            materials = Material.objects.all()
+            return render(
+                request=request, 
+                template_name='materials_main.html', 
+                context={"materials": materials})
+
+        ## Renderizar pagina de un Material
+        else:
+            materials = Material.objects.filter(id=material_id)
+            return render(
+                request=request, 
+                template_name='material.html', 
+                context={"materials": materials})
 
