a7746d89afac5362a7294c3290e66d95ee48c1ff
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-16 19:16:57 -0400

    feat(forum/view): basic view

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Entry, Message
 
 # Create your views here.
+def forum(request, forum_id=None):
+
+    ## TODO: agregar mensajes
+    if request.method == 'GET':
+        if forum_id:
+            forum = Entry.objects.get(id=forum_id)
+            return render(request=request, 
+                          template_name='forum.html',
+                          context={'forum': forum})
+        else:
+            forums = Entry.objects.all()
+            return render(request=request,
+                            template_name='forums_main.html',
+                            context={'forums': forums})
