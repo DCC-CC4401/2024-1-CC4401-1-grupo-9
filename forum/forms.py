d1d1e649dfd7f90d20834b6f028ffcb87314f6be
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-17 12:41:20 -0400

    feat(forum/forms): new form to add a new message

--- forum/forms.py
+++ forum/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from forum.models import Entry, Message
 
 class ForumEntry(forms.Form): #username, password, email, nombre, apellido
+    """ Formulario para agregar entrada de un foro """
 
     title = forms.CharField(max_length=200)
     body = forms.CharField(max_length=1000)
@@ -9,3 +10,11 @@ class ForumEntry(forms.Form): #username, password, email, nombre, apellido
     class Meta:
         model = Entry
         fields = ['title', 'body']
+
+class ForumMessage(forms.Form): #username, password, email, nombre, apellido
+    """ Formulario para agregar mensaje a un foro """
+    message = forms.CharField(max_length=1000)
+    
+    class Meta:
+        model = Message
+        fields = ['message']
