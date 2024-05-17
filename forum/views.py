753b91c258715a0d15a90a3aeaec6feb0ca6cd24
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-17 12:43:34 -0400

    feat(forum/view): added form for the message. MUST BE AUTHENTICATED TO ENTER A FORUM.

--- forum/views.py
+++ forum/views.py
@@ -1,33 +1,64 @@
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, HttpRequest
 from django.shortcuts import render
 from .models import Entry, Message
-from .forms import ForumEntry
+from .forms import ForumEntry, ForumMessage
 from django.contrib import messages
 from django.shortcuts import render, redirect
-# Create your views here.
-def forum(request, forum_id=None):
 
-    ## TODO: agregar mensajes
+
+
+def forum(request: HttpRequest, entry_id: int = None) -> (HttpResponseRedirect | HttpResponsePermanentRedirect):
+    """ Vista para manejar los foros """
+
+    if not request.user.is_authenticated:
+        """ Si el usuario no esta autenticado, se redirige al login """
+        return redirect('/login')
+
+
     if request.method == 'GET':
-        if forum_id:
-            forum = Entry.objects.filter(id=forum_id)
+        """ Si el metodo es GET, se renderiza el template correspondiente """
+        if entry_id:
+            """ Si se pasa un id de entrada, se renderiza el template de foro """
+            forum = Entry.objects.filter(id=entry_id)
+            messages = Message.objects.filter(entry_id=entry_id).order_by('date')
             return render(request=request, 
                           template_name='forum.html',
-                          context={'forum': forum})
+                          context={'forum': forum, 
+                                   'messages': messages, 
+                                   'form': ForumMessage})
         else:
-            forums = Entry.objects.all()
+            """ Si no se pasa un id de entrada, se renderiza el template de foros """
+            forums = Entry.objects.all().order_by('date')
             return render(request=request,
-                            template_name='forums_main.html',
-                            context={'forums': forums})
-        
+                          template_name='forums_main.html',
+                          context={'forums': forums, 
+                                   'form': ForumEntry})
+
     elif request.method == 'POST':
-        form = ForumEntry(request.POST)
-        if form.is_valid():
-            entrada = form.save(commit=False)
-            entrada.save()
-            messages.success(request, 'Entrada subida exitosamente')
-            return redirect('/foro/')
+        """ Si el metodo es POST, se procesa el formulario correspondiente """
+
+        if entry_id:
+            """ Si se pasa un id de entrada, se procesa el formulario de mensaje """
+            form = ForumMessage(request.POST)
+            if form.is_valid():
+                message = form.save(commit=False)
+                message.entry_id = entry_id         ## guardar id de la entrada
+                message.user_id = request.user.id   ## guardar id del usuario
+                message.save()
+                messages.success(request, 'Mensaje subido exitosamente')
+                return redirect('/foro/'+entry_id)
+            else:
+                ## TODO: Modificar esto para manejar el caso en el que no es valido
+                return redirect('/foro/'+entry_id)
+
         else:
-            ##Modificar esto para manejar el caso en el que no es valido
-            return render(request, 'forum.html', {
-            'form': ForumEntry
-            })
+            """ Si no se pasa un id de entrada, se procesa el formulario de entrada """
+            form = ForumEntry(request.POST)
+            if form.is_valid():
+                entrada = form.save(commit=False)
+                entrada.save()
+                messages.success(request, 'Entrada subida exitosamente')
+                return redirect('/foro/')
+            else:
+                ## TODO: Modificar esto para manejar el caso en el que no es valido
+                return redirect('/foro/')
