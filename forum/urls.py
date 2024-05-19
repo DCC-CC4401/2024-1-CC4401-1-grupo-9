831530554d9c84674d743eaee5552857ac3ed26c
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-05-19 01:07:41 -0400

    api_forums modificada para obetener el contenido de un foro especifico y sus mensajes

--- forum/urls.py
+++ forum/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('forum/', views.forum, name='forums'),
     path('forum/<int:entry_id>/', views.forum, name='forum'),
     path('api/forums/', views.api_forums, name='api_forums'),
+    path('api/forums/<int:entry_id>/', views.api_forums, name='api_forum')
 ]
--- forum/views.py
+++ forum/views.py
@@ -19,7 +19,8 @@ def forum(request: HttpRequest, entry_id: int = None) -> (HttpResponseRedirect |
             msgs = Message.objects.filter(entry_id=entry_id).order_by('created_at').reverse()
             return render(request=request, 
                           template_name='forum.html',
-                          context={'forum': forum, 
+                          context={'entry_id': entry_id,
+                                   'forum': forum, 
                                    'messages': msgs, 
                                    'form': ForumMessage})
         else:
@@ -39,7 +40,7 @@ def forum(request: HttpRequest, entry_id: int = None) -> (HttpResponseRedirect |
             if form.is_valid():
                 message = form.save(commit=False)
                 message.entry = Entry.objects.filter(id=entry_id)
-                #message.user = request.user
+                message.user = request.user
                 message.save()
                 messages.success(request, 'Mensaje subido exitosamente')
                 return redirect('/forum/'+entry_id)
@@ -52,7 +53,7 @@ def forum(request: HttpRequest, entry_id: int = None) -> (HttpResponseRedirect |
             form = ForumEntry(request.POST)
             if form.is_valid():
                 entrada = form.save(commit=False)
-                #entrada.user = request.user
+                entrada.user = request.user
                 entrada.save()
                 messages.success(request, 'Entrada subida exitosamente')
                 return redirect('/forum/'+str(entrada.id))
@@ -67,17 +68,39 @@ def api_forums(request: HttpRequest) -> JsonResponse:
     """
 
     if request.method == 'GET':
-        """ Si el metodo es GET, se obtienen los foros """
-        forums = Entry.objects.all().order_by('created_at').reverse()
-        title = request.GET.get('title', None)
+        entry_id = request.GET.get('entry_id', None)
+        if entry_id:
+            """ Si se pasa un id de entrada, se obtienen los mensajes """
+            forum = Entry.objects.filter(id=entry_id)
+            if (forum):
+                messages = Message.objects.filter(entry_id=entry_id).order_by('created_at').reverse()
+                data = {
+                    'forum': {
+                    'id': forum[0].id,
+                    'title': forum[0].title,
+                    'body': forum[0].body,
+                    'created_at': forum[0].created_at
+                    },
+                    'messages': [
+                        {'id': msg.id, 'message': msg.message, 'created_at': msg.created_at} 
+                        for msg in messages
+                    ]
+                }
+            else:
+                data = {}
+                
+        else:
+            """ Si el metodo es GET, se obtienen los foros """
+            forums = Entry.objects.all().order_by('created_at').reverse()
+            title = request.GET.get('title', None)
 
-        """ Si se pasa un titulo, se filtran los foros por el titulo """
-        if title:
-            forums = forums.filter(title__icontains=title)
+            """ Si se pasa un titulo, se filtran los foros por el titulo """
+            if title:
+                forums = forums.filter(title__icontains=title)
 
-        data = [
-            {'id': forum.id, 'title': forum.title, 'body': forum.body, 'created_at': forum.created_at} 
-            for forum in forums
-        ]
+            data = [
+                {'id': forum.id, 'title': forum.title, 'body': forum.body, 'created_at': forum.created_at} 
+                for forum in forums
+            ]
         
         return JsonResponse(data, safe=False)
