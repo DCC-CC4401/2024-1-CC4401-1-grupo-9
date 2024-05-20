7a08fd95697c9ab382a954379f937800350a400c
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-05-20 15:37:35 -0400

    Implementado fetch en el script forum_entry.js para obtener y mostrar la información del foro utilizando la API. Actualizada la plantilla HTML para incluir el contenedor del foro y los mensajes. Ahora, el JavaScript se ejecuta correctamente y muestra los datos del foro y sus mensajes en la página.

--- forum/static/js/forum_entry.js
+++ forum/static/js/forum_entry.js
@@ -1,16 +1,42 @@
-forum_id = window.location.pathname.split('/')[2];
-console.log(forum_id)
+const forum_id = window.location.pathname.split('/')[2];
 
 const fetch_forum = async (id) => {
     try{
-        const url = `/api/forums/${id}`;
+        const url = `/api/forums/?entry_id=${id}`;
         const response = await fetch(url);
         return response.json();
     } catch (error) {
         throw new Error('Error al obtener el foro');
-    }
+    }.0
 }
 
+fetch_forum(forum_id).then((data) => {
+    document.getElementById('entry').innerHTML = 
+        `<div class="entry">
+            <div class="entry-data">
+                <div> Votos: +${5} </div>
+                <div> Respuestas: ${10} </div>
+            </div>
+            <div class="entry-content">
+                <div class="entry-title">${data.forum.title} </div>
+                <div class="entry-body">${data.forum.body} </div>
+            </div>
+        </div>`;
+
+    for (let i = 0; i < data.messages.length; i++) {
+        document.getElementById('entry-answer').innerHTML += 
+            `<div class="entry">
+                <div class="entry-content">
+                    <div class="entry-message"> ${data.messages[i].message} </div>
+                </div>
+                <div class="entry-data">
+                    <div> Votos: +${5} </div>
+                    <div> Respuestas: ${10} </div>
+                </div>
+            </div>`;
+    }
+});
+ 
 
 
 
