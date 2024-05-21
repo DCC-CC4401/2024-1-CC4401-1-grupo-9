175de7e4a7d2933328a97b2d4e146311b9d8a982
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-05-21 01:31:45 -0400

    Creación de estilos CSS específicos para la página del foro. Actualización de la plantilla HTML forum_entry  para incluir contenedores adecuados y aplicar los nuevos estilos. Modificación del Javascript forum_entry para los datos obtenidos desde la base de datos de forma estilizada en la página.

--- forum/static/js/forum_entry.js
+++ forum/static/js/forum_entry.js
@@ -7,33 +7,42 @@ const fetch_forum = async (id) => {
         return response.json();
     } catch (error) {
         throw new Error('Error al obtener el foro');
-    }.0
+    }
 }
 
 fetch_forum(forum_id).then((data) => {
-    document.getElementById('entry').innerHTML = 
-        `<div class="entry">
-            <div class="entry-data">
-                <div> Votos: +${5} </div>
-                <div> Respuestas: ${10} </div>
-            </div>
-            <div class="entry-content">
-                <div class="entry-title">${data.forum.title} </div>
-                <div class="entry-body">${data.forum.body} </div>
-            </div>
-        </div>`;
+    title = document.getElementById('entry-title');
+    h2 = document.createElement('h2');
+    h2.innerHTML = data.forum.title;
+    title.appendChild(h2);
+
+    entry_body = document.getElementById('entry-body');
+    p = document.createElement('p');
+    p.innerHTML = data.forum.body;
+    entry_body.appendChild(p);
+
+    document.getElementById('num-answers').innerText = "Respuestas: " + data.messages.length;
+
+
+    /* ${data.forum.body} */
+
+    
 
     for (let i = 0; i < data.messages.length; i++) {
-        document.getElementById('entry-answer').innerHTML += 
-            `<div class="entry">
-                <div class="entry-content">
-                    <div class="entry-message"> ${data.messages[i].message} </div>
-                </div>
-                <div class="entry-data">
-                    <div> Votos: +${5} </div>
-                    <div> Respuestas: ${10} </div>
-                </div>
-            </div>`;
+        document.getElementById('answer-container').innerHTML += 
+        `<div id="answers" class="answers">
+            <div id="answer-author-time" class="answer-author-time">
+                <p> Subido por Usuario Genérico2 hace 2 horas </p>
+            </div>
+            <div id="answer-content" class="answer-content"> 
+                <p class="entry-message"> ${data.messages[i].message} </p>
+            </div>
+            <div id="answer-buttons" class="answer-buttons">
+                <button id="upvote" class="upvote"> Upvote </button>
+                <span class="votes-counter"> +1 </span>
+                <button id="downvote" class="downvote"> Downvote </button>
+            </div>
+        </div>`;
     }
 });
  
