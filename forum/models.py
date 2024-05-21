75209cea122cd87650a0a885b2380a6810e27120
juaco <joaquin.figueroa.m@ug.uchile.cl>
2024-05-21 18:43:55 -0400

    fix: resolución de conflictos

--- forum/models.py
+++ forum/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from accounts.models import Estudiante
 
 
 class Entry(models.Model):
     """ Es la entrada de un foro """
     title = models.CharField(max_length=200)
     body = models.TextField(max_length=1000)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(Estudiante, on_delete=models.CASCADE, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     ## agregar material
@@ -19,7 +20,7 @@ class Message(models.Model):
     """ Es un mensaje en una entrada de un foro """
     message = models.TextField(max_length=1000)
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(Estudiante, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
