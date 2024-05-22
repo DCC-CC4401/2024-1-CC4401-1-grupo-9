d87c7c4d8c8b0e6399bfec02274cdec3d9d2a3e4
juaco <joaquin.figueroa.m@ug.uchile.cl>
2024-05-22 13:30:06 -0400

    docs: Documentación de accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import Estudiante
 
+"""
+Formulario de ingreso de credenciales de la aplicación
+"""
 class Login(forms.Form):
     username = forms.CharField(label='Username', max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
+"""
+Formulario de Registro de la aplicación
+"""
 class Register(UserCreationForm): #username, password, email, nombre, apellido
     username = forms.CharField(label='Username', max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     #password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
@@ -15,6 +21,10 @@ class Register(UserCreationForm): #username, password, email, nombre, apellido
     class Meta:
         model = Estudiante
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+"""
+Formulario de ayuda por perdida de contraseña
 
+(Implementación final a futuro)
+"""
 class ForgetPassword(forms.Form):
     username_l = forms.CharField(label='Username', max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
--- accounts/views.py
+++ accounts/views.py
@@ -5,27 +5,31 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+"""
+Vista que permite recibir una petición de login para que un usuario ingrese y se autentifique
+Puede recibir:
+    Get: para renderizar la plantilla login.html
+    Post: para enviar credenciales de autenticación
+"""
 def login_view(request):
+    #Caso GET
     if request.method == 'GET':
         #mostramos la pagina
         return render(request, 'login.html', {
         'form': Login
         })
 
+    #Caso POST
     elif request.method == 'POST':
         form = Login(request.POST)
         #validamos los datos
-        print(form.is_valid())
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            print(f"El nombre de usuario es {username} ")
+            #Autenticamos
             user = authenticate(request, username=username, password=password)
-            print(user)
             if user is not None:
                 login(request, user)  # Iniciar la sesión del usuario
-                print(f"Usuario {username} autenticado exitosamente")
                 return redirect('/profile/') 
         else:
             #aviso y que intente de nuevo
@@ -33,14 +37,21 @@ def login_view(request):
             'form': Login
             })
 
+"""
+Vista que permite recibir una petición de resgistro para que una persona cree un usuario
+Puede recibir:
+    Get: para renderizar la plantilla register.html
+    Post: para enviar datos de creación de usuario
+"""
 def register_view(request):
+    #Caso GET
     if request.method == 'GET':
         #mostramos la página
         return render(request, 'register.html', {
             'form': Register
         })
 
-    
+    #Caso POST
     elif request.method == 'POST':
         print(request.POST)
         form = Register(request.POST)
@@ -49,27 +60,31 @@ def register_view(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
+            #Registramos al usuario
             user.save()
             messages.success(request, 'Te registraste EXITOsamente')
             login(request,user)
-            # acá se agrega un nuevo estudiante a la db
-            # Estudiante.objects.create(username = request.POST['username'],
-            #                           password = request.POST['password1'],
-            #                           rol = "1",
-            #                           email = request.POST['email'])
-            # probablemente redirigimos al login
             return redirect('/login/')
         else:
             return render(request, 'register.html', {
             'form': Register
             })
 
+"""
+Vista para acceder al perfil de un usuario
+Existen dos casos:
+    1) No se entrega un id de usuario en la url: En este caso se devuelve el perfil del usuario que 
+    realiza la petición.
+    2) Se entrega un id de usuario en la url: En este caso se devuelve el perfil del usuario respectivo
+    de acuerdo a el id de la url.
+"""
 @login_required
 def profile(request,user_id=None):
-    
     if request.method == 'GET':
+        #Accedemos al perfil del usuario del id de la url
         if user_id:
             estudiante = Estudiante.objects.get(id = user_id)
+        #Accedemos al perfil del estudiante que realiza la request
         else:
             estudiante = request.user
 
@@ -78,10 +93,20 @@ def profile(request,user_id=None):
         }
         return render(request, 'profile.html',context)
 
+"""
+Vista que renderiza plantilla forgot-password.html
+
+(Implementación futura)
+"""
 def forgot_password_view(request):
     return render(request, 'forgot-password.html', {
         'form': ForgetPassword
     })
 
+"""
+Vista que permite acceder a la plantilla home.html
+
+(Implementación futura)
+"""
 def home_view(request):
     return render(request, 'home.html')
