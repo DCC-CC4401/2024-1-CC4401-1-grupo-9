6274963a54ca4a9bec7d50336992a7d4bdbd1572
chunchometalero <juan.valdivia.g@ug.uchile.cl>
2024-07-03 00:44:33 -0400

    feat: added urls for api that controls messages and entries votes
    Co-authored-by: Marco Martínez <marcotinez@users.noreply.github.com>

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('forum/', views.forum, name='forums'),
     path('forum/<int:entry_id>/', views.forum, name='forum'),
     path('api/forums/', views.api_forums, name='api_forums'),
-    path('api/forums/<int:entry_id>/', views.api_forums, name='api_forum')
+    path('api/forums/<int:entry_id>/', views.api_forums, name='api_forum'),
+    path('api/vote/', views.api_votes, name='api_votes'),
+    path('api/vote/<int:entry_id>/', views.api_votes, name='api_vote')
 ]
