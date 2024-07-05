c0b963142a54674577261fdb1c6b1d710492e18c
marcotinez <marco.martinez.s@ug.uchile.cl>
2024-07-05 14:46:38 -0400

    feat: new form to upload material

--- material/forms.py
+++ material/forms.py
@@ -2,6 +2,18 @@ from django import forms
 from .models import Material
 
 class MaterialForm(forms.ModelForm):
+    # Tipos de materiales
+    MATERIAL_TYPE_CHOICES = [
+        ('Auxiliar', 'Auxiliar'),
+        ('Control', 'Control'),
+        ('Tutoría', 'Tutoría'),
+    ]
+    # Años
+    YEAR_CHOICES = [(year, year) for year in range(1999, 2024)]
+
+    type = forms.ChoiceField(choices=MATERIAL_TYPE_CHOICES)
+    year = forms.ChoiceField(choices=YEAR_CHOICES)
+
     class Meta:
         model = Material
-        fields = ['name', 'file', 'course']
+        fields = ['file', 'name', 'course', 'type', 'year']
