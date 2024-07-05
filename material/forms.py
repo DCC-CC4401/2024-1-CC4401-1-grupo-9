e7db7a67c56791710bf6f2bfd05a4a65a97a01bd
marcotinez <marco.martinez.s@ug.uchile.cl>
2024-07-05 18:09:24 -0400

    add: file field to improve file selection

--- material/forms.py
+++ material/forms.py
@@ -11,6 +11,7 @@ class MaterialForm(forms.ModelForm):
     # Años
     YEAR_CHOICES = [(year, year) for year in range(1999, 2024)]
 
+    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'class': 'custom-file-input', 'id': 'file-upload'}))
     type = forms.ChoiceField(choices=MATERIAL_TYPE_CHOICES)
     year = forms.ChoiceField(choices=YEAR_CHOICES)
 
