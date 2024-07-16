a8c186f62c5c6da4183392567bea99ba17952d1c
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-07-15 21:42:31 -0400

    fix: material upload

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+MEDIA_URL = '/material/static/Documents/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
--- material/urls.py
+++ material/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('material', views.material, name='material'),
     path('material/<str:material_id>/', views.material, name="specific-material"),
     path('materials/upload/', views.subirMaterial, name='material_upload'),
 ]         
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
