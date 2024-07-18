480cd0aeab2f7e9affbf6edc9a049d1ff6e44ffa
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-07-18 10:53:40 -0400

    fix(material): fixed filters error with js export

--- material/static/js/material-filters.js
+++ material/static/js/material-filters.js
@@ -8,9 +8,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const getCookie_1 = require("./getCookie");
-const csrftoken = (0, getCookie_1.getCookie)('csrftoken');
+const getCookie = (name) => {
+    let cookieValue = "";
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+};
+const csrftoken = getCookie('csrftoken');
 /* The id's of the filters */
 const filtersIds = ["auxiliar-checkbox", "control-checkbox", "tutoria-checkbox"];
 const filtersNames = {
--- material/static/js/material-filters.ts
+++ material/static/js/material-filters.ts
@@ -1,5 +1,17 @@
-import { getCookie } from "./getCookie";
-
+const getCookie = (name: string) : string => {
+    let cookieValue: string = "";
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 const csrftoken = getCookie('csrftoken');
 
 /* The id's of the filters */
